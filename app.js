900133fb44560ed2fb988fb6e0b6323bd9555c38
Adebayo Kehinde <press.spark@gmail.com>
2018-08-16 12:33:05 +0100

    Pretty printing object

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
 request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=%2013%20lombard%20street%20philadephia',
+    url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyD7DsngSv3MQvUv_4EOAWGql-Zyu0LGT08&address=%2013%20lombard%20street%20philadephia',
     json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 })
