78f74a3f5f980ad8ba70ef8a7371264f75b6c412
Adebayo Kehinde <press.spark@gmail.com>
2018-08-17 15:05:37 +0100

    Abstracting callbacks

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs.options({
-    a:{
-        demand:true,
-        alias:'address',
-        address:'Address to fetch weather for',
-        string: true
+// const argv = yargs.options({
+//     a:{
+//         demand:true,
+//         alias:'address',
+//         address:'Address to fetch weather for',
+//         string: true
+//     }
+// })
+// .help()
+// .alias('help','h')
+// .argv;
+
+// var encodedAddress = encodeURIComponent(argv.address);
+
+// geocode.geocodeAddress(encodedAddress, (errorMessage, results) => {
+//  if(errorMessage){
+//      console.log(errorMessage)
+//  } else{
+//     console.log(JSON.stringify(results, undefined, 2));
+//  }
+// });
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/902caa11fa79dc5b14191836ab8d3592/39.472619,-0.367398',
+    json: true
+}, (error,response, body) => {
+    if(!error && response.statusCode === 200){
+        console.log(body.currently.temperature)
     }
+    else {
+        console.log('Unable to fetch weather.');
+    } 
+
 })
-.help()
-.alias('help','h')
-.argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-geocode.geocodeAddress(encodedAddress, (errorMessage, results) => {
- if(errorMessage){
-     console.log(errorMessage)
- } else{
-    console.log(JSON.stringify(results, undefined, 2));
- }
-});
-
-// var options = {
-//     url: 'https://maps.googleapis.com/maps/api/geocode/json?' + 'address=%201,3%20lombard%20street%20philadephia' +
-//     '&key=' + process.env.WEATHER_APP_KEY, json: true
-//   };
